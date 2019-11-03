e47b6056580957fef3a7bbfbcd4c9231ffccba5d
Matheus Alves <matheuseejam@gmail.com>
2019-11-03 16:49:25 -0200

    Iniciando CI bug fix

--- app/Http/Middleware/PermitirCors.php
+++ app/Http/Middleware/PermitirCors.php
@@ -16,9 +16,8 @@ class PermitirCors
      */
     public function handle($request, Closure $next)
     {
-		\header("Access-Control-Allow-Origin: *");
-
         $headers = [
+            "Access-Control-Allow-Origin" => "*",
             'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin, Authorization'
         ];
